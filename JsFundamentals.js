22ecc4801e5f552226fd6cc3478c7cd3afaff8da
RKM170 <kumarmishrarishi8@gmail.com>
2026-05-12 13:23:19 +0530

    basic fundamentals to know before st node js

---
+++ JsFundamentals.js
@@ -0,0 +1,49 @@
+// function value(name){
+//     return "my name is " + name;
+// }
+
+// console.log(value("Rishi"));
+
+// don = (age)=>{
+// return "and my age is "+ age;
+// }
+
+// console.log(don(18));
+// let i =0;
+// while(i <=5){
+//     console.log("number:"+ i);
+//     i++
+// }
+// clg - console.log
+// dar→	destructing array syntax const [first, second] = [1,2]
+
+// let fruits =["Banana","apple","watermelon"];
+
+// console.log(fruits[0]);
+// console.log(fruits[1]);
+// console.log(fruits[2]);
+
+// console.log(fruits);
+
+// fruits.push("mango");
+// console.log(fruits);
+
+// fruits.push("papya");
+
+// for(let a=0; a<fruits.length; a++){
+//     console.log("fruit "+ (a + 1) +  ":" +fruits[a]);
+// }
+
+
+let user={
+
+    name:"Rishi",
+    age:18,
+    Array:['1','2'],
+    
+}
+
+    
+console.log(user.Array);
+console.log(user.age);
+console.log(user.name);
